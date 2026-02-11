775564d683e8fd871e92fc0e83cb6d44de997c3b
Anthony Rossa <anthonysixty23@gmail.com>
2026-02-11 06:14:49 -0300

    bigger card titles and card image width fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer" id="footer">
-      <h4 className="footer__copyright">© 2025 Anthony Rossa</h4>
+      <h4 className="footer__copyright">&#169; 2026 Anthony Rossa</h4>
     </footer>
   );
 }
